df53c730dc424840d030d3fbf8f6b0039a511056
unluckyjung <ybook2012@gmail.com>
2022-06-22 23:59:51 +0900

    feat: @RequestParam 생략 예제 추가

--- crud-app/src/main/kotlin/com/example/mvc/controller/GetController.kt
+++ crud-app/src/main/kotlin/com/example/mvc/controller/GetController.kt
@@ -39,18 +39,37 @@ class GetController {
         return "Hello $name your age is $age and your phone-number is $phoneNumber"
     }
 
+
+    // name null = 500 에러 + NPE (kotlin not null)
+    // age null = 400 에러 (required = false)
+    @GetMapping("hello/query-params/without-annotation")
+    fun helloQueryParamsWithoutAnnotation(
+        name: String,
+        @RequestParam age: Int,
+    ): String {
+        return "name: $name, age: $age"
+    }
+
     // http://localhost:8080/api/v1/hello/query-params/object?age=30&name=goodall&phoneNumber=010-1234-5678
     // get + requestBody 방식이 아님.
     @GetMapping("/hello/query-params/object")
-    fun helloQueryParamsObject(memberRequest: Member.Request): ResponseEntity<Member.Response> {
+    fun helloQueryParamsObject(@ModelAttribute memberRequest: Member.Request): ResponseEntity<Member.Response> {
         return ResponseEntity.ok(
             Member.Response(
-                "hello ${memberRequest.name}",
-                memberRequest.age,
-                memberRequest.phoneNumber
+                "hello ${memberRequest.name}", memberRequest.age, memberRequest.phoneNumber
             )
         )
     }
+
+    @GetMapping("/hello/query-params/object/without-annotation")
+    fun helloQueryParamsObjectWithoutAnnotation(memberRequest: Member.Request): ResponseEntity<Member.Response> {
+        return ResponseEntity.ok(
+            Member.Response(
+                "hello ${memberRequest.name}", memberRequest.age, memberRequest.phoneNumber
+            )
+        )
+    }
+
 
     @GetMapping("hello/query-params/map")
     fun helloQueryParamsMap(@RequestParam request: Map<String, Any>): Map<String, Any> {
